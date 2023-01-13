9d8f5b16e776e22df6694d61179d9170bf72bb0c
hcdiekmann <hcdiekmann@hotmail.com>
2023-01-13 15:05:40 +0100

    minor changes to structure

--- devkit_object_detection/devkit_object_detection/detect.py
+++ devkit_object_detection/devkit_object_detection/detect.py
@@ -3,8 +3,12 @@
 import time
 import rclpy                                                # Python client library for ROS 2
 from rclpy.node import Node                                 # Handles the creation of ROS nodes
-from sensor_msgs.msg import Image                           # Image is the ROS message type 
+from sensor_msgs.msg import Image                           # Image is the ROS message type
+from std_msgs.msg import Header, String, Bool
+from geometry_msgs.msg import PoseStamped
+
 from cv_bridge import CvBridge                              # Package to convert between ROS and OpenCV Images
+br = CvBridge()
 
 import numpy as np
 import torch
@@ -22,30 +26,31 @@ class ObjectDetector(Node):
         """
         # Initiate the Node class's constructor and give node name
         super().__init__('object_detector')
-        self.br = CvBridge()
         self.color_frame = None
         self.depth_frame = None
         self.depth_array = None
 
-        # Create the camera subscribers, Note: The RS D405 model publishes to /camera/color/image_rect_raw
-        self.color_sub = self.create_subscription(Image,'/camera/color/image_raw', self.color_callback, 10) 
-        self.depth_sub = self.create_subscription(Image,'/camera/aligned_depth_to_color/image_raw', self.depth_callback, 10)
-        self.inference_pub = self.create_publisher(Image, '/camera/inference_image', 10)
-
         # Load and configure YOLO model
         self.model = torch.hub.load('ultralytics/yolov5', 'yolov5s6')
-        self.modeliou = 0.45            # NMS IoU threshold (overlapping of boxes)
+        self.model.iou = 0.45           # NMS IoU threshold (overlapping of boxes)
         self.model.conf = 0.40          # NMS confidence threshold
         self.model.multi_label = False  # NMS multiple labels per box
         self.model.classes = None       # (optional list) filter by class, i.e. = [0, 15, 16] for COCO persons, cats and dogs
         self.model.max_det = 1000       # maximum number of detections per image
+
+        # Create the camera subscribers, Note: The RealSense D405 model publishes rgb_image to /camera/color/image_rect_raw
+        self.color_sub = self.create_subscription(Image,'/camera/color/image_raw', self.color_callback, 10) 
+        self.depth_sub = self.create_subscription(Image,'/camera/aligned_depth_to_color/image_raw', self.depth_callback, 10)
+        
+        # Create publishers for the inference_image and object markers
+        self.inference_pub = self.create_publisher(Image, '/camera/inference_image', 10)
 
 
     def color_callback(self, data):
         """
         Callback function for the color frame.
         """
-        ros_image = self.br.imgmsg_to_cv2(data)
+        ros_image = br.imgmsg_to_cv2(data)
         self.color_frame = cv2.cvtColor(ros_image, cv2.COLOR_BGR2RGB)
          
 
@@ -53,7 +58,7 @@ class ObjectDetector(Node):
         """
         Callback function for the depth frame.
         """
-        ros_depth_frame = self.br.imgmsg_to_cv2(data)
+        ros_depth_frame = br.imgmsg_to_cv2(data)
         self.depth_frame = cv2.applyColorMap(cv2.convertScaleAbs(ros_depth_frame, alpha=0.08), cv2.COLORMAP_JET)
         self.depth_array = np.array(ros_depth_frame, dtype=np.float64)
         self.detect()
@@ -62,19 +67,16 @@ class ObjectDetector(Node):
 
 
     def detect(self):
-        info_string = ""
-        t1 =  time.process_time()
         results = self.model(self.color_frame)
-        results.print()
-        t2 =  time.process_time()
-        #print(f'{info_string}Inference time: ({(1E3 * (t2 - t1)):.1f}ms) {results.print()}')
-        
-        self.publish_objects(results)
+        results.print() # prints inference metrics to terminal
+        self.process_predictions(results)
+        inference_image = np.squeeze(results.render()) # draws bounding boxes with labels and confidence on color frame
+        self.inference_pub.publish(inference_image) # publish inference image to ros topic
 
-        cv2.imshow('Prediction Result', np.squeeze(results.render())) #draws bounding boxes with labels and confidence   
-        cv2.waitKey(1)
+        #cv2.imshow('Inference Image', inference_image )   
+        #cv2.waitKey(1)
 
-    def publish_objects(self, predictions):
+    def process_predictions(self, predictions):
         objects = predictions.pandas().xyxy[0].to_dict(orient = "records")
         
         if len(predictions) != 0:
@@ -91,6 +93,7 @@ class ObjectDetector(Node):
                 cv2.putText(self.color_frame, "{}mm".format(distance),(object_center[0], object_center[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 2, cv2.LINE_AA)
 
 
+
 def main(args=None):
     rclpy.init(args=args)
 
@@ -103,7 +106,7 @@ def main(args=None):
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    image_subscriber.destroy_node()
+    detection_node.destroy_node()
 
     # Shutdown the ROS client library for Python
     rclpy.shutdown()
