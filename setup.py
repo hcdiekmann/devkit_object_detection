481f465f618de2c9b080ef28af6426167ed66720
hcdiekmann <hcdiekmann@hotmail.com>
2023-01-25 14:25:13 +0100

    add comma

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-'detection_node = devkit_object_detection.obj_detection_node:main'
+'detection_node = devkit_object_detection.obj_detection_node:main',
 'obj_tf_pub_node = devkit_object_detection.tf_publisher_node:main'
         ],
     },
